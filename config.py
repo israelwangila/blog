f0ad27269d784f2b8e9b96b5d907a971acbf2337
israelwangila <wangilaisrael@gmail.com>
2020-09-28 11:44:19 +0300

    configuration

---
+++ config.py
@@ -0,0 +1,33 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://alex:alex@localhost/bpost'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
